0216ab90fc58af81a47cce1b0834e23b596deb18
GreteLaw <glawre19@students.kennesaw.edu>
2023-11-18 16:44:00 -0500

    updates made to sample code

--- globalentry.py
+++ globalentry.py
@@ -1,25 +1,16 @@
-import datetime
+import requests
 from datetime import datetime, timedelta
 
-def program_email(subject, body)
+def program_email(subject, body):
     system_email_address = "Scans4U@ ProScan.com"
     system_password= "Password7540fa"
     patient_email= "Patient23@gmail.com"
 
-    print("Notification as requested to email", patient_email + :\n, subject, \n body)
-# To locations with available appts, will need to scrape location availability or create list
-# locations = requests.get("https://ttp.cbp.dhs.gov/schedulerapi/slots/asLocations?limit=100").json()
+    print("Notification as requested to email", patient_email + ":\n Subject" +"\n Body:",body)
 
-# names_ids = {}
+# To find locations with available appts, will need to scrape location availability or create list
 
-# for i in locations:
-#     names_ids[i['name']] = i['id']
-
-# put your email here
 you = ''
-# currently hardcoded for the Boston location, will update
-URL = "https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId=5441&minimum=1"
-
 """
 Uncomment this section if you wanted to query for the next N days instead of the soonest available.
 """
@@ -27,7 +18,7 @@ Uncomment this section if you wanted to query for the next N days instead of the
 # start = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M')
 # end = (datetime.datetime.now() + datetime.timedelta(days=N)).strftime('%Y-%m-%dT%H:%M')
 
-# URL = "https://ttp.cbp.dhs.gov/schedulerapi/locations/5024/slots?startTimestamp={}&endTimestamp={}&limit=100".format(start, end)
+URL = "https://radiologyassist.com/ra-search-results/".format("")
 
 init = datetime.now() - timedelta(seconds=1)
 
@@ -51,9 +42,9 @@ while True:
         init = datetime.now()
         if len(appt) > 0:
             print("There is an appointment available.")
-        # if len(appt) > 0:
-            # sendEmail(appt)
-            # break
+        if len(appt) > 0:
+            #sendEmail(appt)
+            break
         else: 
-            print("no appt found",appt, init)
+            print("No Appointment found.",appt, init)
             break

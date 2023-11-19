0cdab919303c1a8cd37e427c9230297c7b876d61
GreteLaw <glawre19@students.kennesaw.edu>
2023-11-18 20:35:19 -0500

    script for email functionality added to code guesspass used to mask password entry

--- globalentry.py
+++ globalentry.py
@@ -1,13 +1,33 @@
 import requests
 from datetime import datetime, timedelta
+import smtplib
+from email.message import EmailMessage
+from getpass import getpass
 
-def program_email(subject, body):
-    system_email_address = "Scans4U@ ProScan.com"
-    system_password= "Password7540fa"
-    patient_email= "Patient23@gmail.com"
+def get_email_secure():
+    email = input("Enter program email:")
+    password = getpass("Enter password")
+    return email, password
 
-    print("Notification as requested to email", patient_email + ":\n Subject" +"\n Body:",body)
 
+def send_email(appt, email, password):
+    date = datetime.strptime(appt[0]['startTimestamp'], '%Y-%m-%dT%H:%M')
+    text = "Hello. There is an available radiology appointment at {wkday}, {mth} {d} at {time}.".format(wkday=date.strftime('%A'), mth=date.strftime('%B'), d=date.strftime('%d'), time=date.strftime('%I:%M %p'))
+    msg = EmailMessage()
+    msg.set_content(text)
+    msg['Subject'] = 'Radiology Appointment Availability'
+    msg['From'] = 'ProScan@radiology.com'
+    msg['To'] = email
+
+    with smtplib.SMTP_PORT('smtp.example.com', 587) as server:
+        server.starttls()
+        server.login(email,password)
+        server.send_message(msg)
+
+def check_appointment():
+    URL = "https://radiology-scheduler-api.com/slots?orderBy=soonest&limit=1&locationId=1234&minimum=1"
+    init = datetime.now() - timedelta(seconds=1)
+    
 # To find locations with available appts, will need to scrape location availability or create list
 
 you = ''
@@ -18,22 +38,11 @@ Uncomment this section if you wanted to query for the next N days instead of the
 # start = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M')
 # end = (datetime.datetime.now() + datetime.timedelta(days=N)).strftime('%Y-%m-%dT%H:%M')
 
-URL = "https://radiologyassist.com/ra-search-results/".format("")
+URL = 1
 
 init = datetime.now() - timedelta(seconds=1)
 
-# working on the email functionality
-# def sendEmail(appt):
-  #     date = datetime.strptime(appt[0]['startTimestamp'], '%Y-%m-%dT%H:%M')
-  #     text = "Hello! There is an available appointment at Boston Logan Airport at {wkday}, {mth} {d} at {time}.".format(wkday=date.strftime('%A'), mth=date.strftime('%B'), d=date.strftime('%d'), time=date.strftime('%I:%M %p'))
-  #     msg = EmailMessage()
-  #     msg.set_content(text)
-  #     msg['Subject'] = f'Global Entry Appointment Alert!'
-  #     msg['From'] = 'alert@globalentry.com'
-  #     msg['To'] = you
-  #     s = smtplib.SMTP('localhost')
-  #     s.send_message(msg)
-  #     s.quit()
+
 
 while True:
     if (datetime.now() - init > timedelta(seconds=1)):
@@ -43,7 +52,7 @@ while True:
         if len(appt) > 0:
             print("There is an appointment available.")
         if len(appt) > 0:
-            #sendEmail(appt)
+            send_email(appt)
             break
         else: 
             print("No Appointment found.",appt, init)

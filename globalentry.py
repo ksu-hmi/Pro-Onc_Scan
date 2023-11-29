13e9f194b76bd45fcf24967d54ebc0e48b675ef5
GreteLaw <glawre19@students.kennesaw.edu>
2023-11-28 22:53:20 -0500

    Update globalentry.py

--- globalentry.py
+++ globalentry.py
@@ -13,30 +13,51 @@ def get_email_secure():
 def send_email(appt, email, password):
     date = datetime.strptime(appt[0]['startTimestamp'], '%Y-%m-%dT%H:%M')
     text = "Hello. There is an available radiology appointment at {wkday}, {mth} {d} at {time}.".format(wkday=date.strftime('%A'), mth=date.strftime('%B'), d=date.strftime('%d'), time=date.strftime('%I:%M %p'))
+
     msg = EmailMessage()
     msg.set_content(text)
-    msg['Subject'] = 'Radiology Appointment Availability'
-    msg['From'] = 'testmailbox2024.com'
-    msg['To'] = 'glawre19@students.kennesaw.edu'
+    msg['Subject'] = "Radiology Appointment Availability"
+    msg['From'] = "testmailbox2024.com"
+    msg['To'] = "glawre19@students.kennesaw.edu"
 
 try:  
-    with smtplib.SMTP_PORT('smtp.gmail.com', 587) as server:
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
-        server.login(email,password)
+        server.login(email, password)
         server.send_message(msg)
 except Exception as e:
     print('An error occurred while sending email:')
 
 def check_appointment():
-    URL = "https://radiology-scheduler-api.com/slots?orderBy=soonest&limit=1&locationId=1234&minimum=1"
+    URL = "None"
     last_check = datetime.now() - timedelta(seconds=1)
     
+    while True:
+        if (datetime.now() - last_check > timedelta(seconds=1)):
+            try:
+                appt = requests.get(URL).json()
+             # there exists an appointment
+                last_check = datetime.now()
+                
+                if len(appt) > 0:
+                    print("There is an appointment available.")
+                    send_email(appt, email, password)
+                    break
+                else: 
+                    print("No Appointment found.",appt, last_check)
+                    break
+            except requests.RequestException as e:
+                print("Error during API request:{e}")
+if __name__ == "__main __":
+    email, password = get_email_secure()
+    check_appointment()
+
 ## To find locations with available appts, will need to scrape location availability or create list
 #replace 'smtp.example.com' with the appropriate SMTP server for your email provider
 #find correct SMTP server and port number (587) email provider, you usually need to consult the documentation online
 #create and add email address
 
-you = ''
+#you = ''
 """
 Uncomment this section if you wanted to query for the next N days instead of the soonest available.
 """
@@ -44,25 +65,3 @@ Uncomment this section if you wanted to query for the next N days instead of the
 # start = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M')
 # end = (datetime.datetime.now() + datetime.timedelta(days=N)).strftime('%Y-%m-%dT%H:%M')
 
-URL = 1
-
-last_check = datetime.now() - timedelta(seconds=1)
-
-
-
-while True:
-    if (datetime.now() - last_check > timedelta(seconds=1)):
-        appt = requests.get(URL).json()
-        # there exists an appointment
-        last_check = datetime.now()
-
-        if len(appt) > 0:
-            print("There is an appointment available.")
-        if len(appt) > 0:
-            send_email(appt)
-            break
-        else: 
-            print("No Appointment found.",appt, last_check)
-            break
-
-check_appointment()

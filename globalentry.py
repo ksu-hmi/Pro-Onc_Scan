71d820992b6b42a88ef875e58aeac6d463888748
GreteLaw <glawre19@students.kennesaw.edu>
2023-12-06 00:03:49 -0500

    Additions to code

--- globalentry.py
+++ globalentry.py
@@ -9,6 +9,8 @@ def get_email_secure():
     password = getpass("Enter password")
     return email, password
 
+def get_password():
+    return getpass("Enter your email password: ")
 
 def send_email(Location, Date, Time, email, password):
     text = "Hello. There is an available radiology appointment at {Date} {Time} at {Location}."
@@ -31,23 +33,20 @@ def check_appointment():
     csv_path = "C:\Users\grete\OneDrive - Kennesaw State University\HMI 7540-Hlthcare Info Sys. Fall 2023\mock_appointments.csv"
     last_check = datetime.now() - timedelta(seconds=1)
     
-    while True:
-        if (datetime.now() - last_check > timedelta(seconds=1)):
-            try:
-                appts_df = pd.read_csv(csv_path)
+    try:
+        appts_df = pd.read_csv(csv_path)
+        if not appts_df.empty:
+            appt = appts_df.iloc[0]
+            Location = appt['Location']
+            Date = appt['Date']
+            Time = appt['Time']
 
-                if not appts_df.empty:
-                    appt = appts_df.iloc[0]
-                    Location = appt['Location']
-                    Date =appt['Date']
-                    Time = appt['Time']
-                print("There is an appointment available: Location=", Location + "Date= "Date, "Time= ",Time)
-                send_email(location, date, time, email, password)
-                break
+            print("There is an appointment available: Location=", Location + "Date= "+Date, "Time= "+ Time)
+            
+            send_email(location, date, time, email, password)
         else:
             print("No Appointment found.")
-            break
-        except Exception as e:
+    except Exception as e:
         print("Error during appointment check:{e}")
         
 if __name__ == "__main __":
